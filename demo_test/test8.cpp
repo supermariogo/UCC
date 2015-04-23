486ced4fb92f52de3e11ce81c3be0d7cad2939ee
mac_vbox <maoze365@gmail.com>
2015-04-23 16:55:49 -0700

    add test8

--- demo_test/test8.cpp
+++ demo_test/test8.cpp
@@ -1,4 +1,26 @@
-int main(){
+int main()  // cc4 is 5
+{
+    bool a = false;
+    bool b = true;
 
-	
+    if(a==1){
+        if(b==1){
+        	if(c==1){
+
+        	}
+        	if(d==1){
+
+        	}
+
+        }
+        
+        	
+    }
+
+    if(a==1 && b==1 && c==1){
+    	cout << "aaa";
+    }
+    if(a==1 && b==1 && d==1){
+    	cout << "aaa";
+    }
 }
